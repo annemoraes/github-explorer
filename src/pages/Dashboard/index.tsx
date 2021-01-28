e092772037c4f6c7f2c7cbc870b2e8862eec841d
annemoraes <ilarymoraes@hotmail.com>
2021-01-28 18:12:07 -0300

    repositorios estaticos

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,52 @@
 import React from 'react';
-import { Title } from './styles';
+
+import { FiChevronRight } from 'react-icons/fi';
+import LogoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositorios</Title>;
+  return (
+    <>
+      <img src={LogoImg} alt="Github" />
+      <Title>Explore repositorios</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img src="https://github.com/annemoraes.png" alt="Anne Moraes" />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img src="https://github.com/annemoraes.png" alt="Anne Moraes" />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img src="https://github.com/annemoraes.png" alt="Anne Moraes" />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
