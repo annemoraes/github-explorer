1512c473a55519705afb1023e4657fc299c9ae4f
annemoraes <ilarymoraes@hotmail.com>
2021-01-28 16:23:24 -0300

    rotas e estilo global

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return <div className="App" />;
-}
+import Routes from './routes';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
